344eaa1d2d69a7b1595060bb277047c47f32982b
a-know <a.know.3373@gmail.com>
2015-07-13 21:47:28 +0900

    Edit fizz_buzz.rb

--- jobs/fizz_buzz.rb
+++ jobs/fizz_buzz.rb
@@ -1,4 +1,47 @@
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '1m', :first_in => 0 do |job|
-  send_event('widget_id', { })
+class FizzBuzz
+  FIZZ=3
+  BUZZ=5
+
+  def initialize
+    @current_num = 1
+  end
+
+  def fizzbuzz
+    if fizzbuzz?
+      out = "fizzbuzz"
+    elsif fizz?
+      out = "fizz"
+    elsif buzz?
+      out = "buzz"
+    else
+      out = @current_num
+    end
+    go_next
+    out
+  end
+
+  private
+
+  def go_next
+    @current_num+=1
+  end
+
+  def fizz?
+    (@current_num % 3) == 0
+  end
+
+  def buzz?
+    (@current_num % 5) == 0
+  end
+
+  def fizzbuzz?
+    (@current_num % (FIZZ*BUZZ)) == 0
+  end
+
+end
+
+fb = FizzBuzz.new
+
+SCHEDULER.every '5s', :first_in => 0 do
+  send_event('fizz_buzz', { value: fb.fizzbuzz })
 end
