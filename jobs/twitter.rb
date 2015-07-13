0468a4370600452fc9a196fd6472f46a42690c55
a-know <a.know.3373@gmail.com>
2015-07-13 22:36:04 +0900

    bundle exec dashing install 9aa0823efdfc085f675d

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -5,24 +5,23 @@ require 'twitter'
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
 twitter = Twitter::REST::Client.new do |config|
   config.consumer_key = 'YOUR_CONSUMER_KEY'
-  config.consumer_secret = 'YOUR_CONSUMER_SECRET'
-  config.access_token = 'YOUR_OAUTH_TOKEN'
-  config.access_token_secret = 'YOUR_OAUTH_SECRET'
+  config.consumer_secret = 'YOUR_CONSUMER_SECRET_KEY'
+  config.access_token = 'YOUR_ACCESS_TOKEN'
+  config.access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
 end
 
-search_term = URI::encode('#todayilearned')
-
-SCHEDULER.every '10m', :first_in => 0 do |job|
+SCHEDULER.every '15m', :first_in => 0 do |job|
   begin
-    tweets = twitter.search("#{search_term}")
-
-    if tweets
-      tweets = tweets.map do |tweet|
+    user = twitter.user
+    if mentions
+      mentions = mentions.map do |tweet|
         { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https }
       end
-      send_event('twitter_mentions', comments: tweets)
-    end
+
+    send_event('twitter_mentions', {comments: mentions})
+    end    
   rescue Twitter::Error
-    puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
+    puts "\e[33mThere was an error with Twitter\e[0m"
   end
+
 end
