e236402beaddb07c7ade5c9d51e7060e9c1e28d4
a-know <a.know.3373@gmail.com>
2015-08-08 11:22:27 +0900

    試作

---
+++ jobs/aiben.rb
@@ -0,0 +1,42 @@
+require 'google/api_client'
+require 'google/api_client/client_secrets'
+require 'google/api_client/auth/file_storage'
+require 'google/api_client/auth/installed_app'
+require 'google_drive'
+require 'pp'
+
+CREDENTIAL_STORE_FILE = "credential-oauth2.json"
+
+def setup()
+  client = Google::APIClient.new(:application_name => 'Get Value from Google SpreadSheet with Dashing',
+      :application_version => '1.0.0')
+
+  file_storage = Google::APIClient::FileStorage.new(CREDENTIAL_STORE_FILE)
+  if file_storage.authorization.nil?
+    flow = Google::APIClient::InstalledAppFlow.new(
+      :client_id => ENV['GOOGLE_DRIVE_CLIENT_ID'],
+      :client_secret => ENV['GOOGLE_DRIVE_CLIENT_SECRET'],
+      :scope => %w(
+        https://www.googleapis.com/auth/drive
+        https://docs.google.com/feeds/
+        https://docs.googleusercontent.com/
+        https://spreadsheets.google.com/feeds/
+      ),
+    )
+    client.authorization = flow.authorize(file_storage)
+  else
+    client.authorization = file_storage.authorization
+  end
+
+  return client
+end
+
+client = setup()
+
+SCHEDULER.every '10s', :first_in => 0 do |job|
+  session = GoogleDrive.login_with_oauth(client.authorization.access_token)
+  ws = session.spreadsheet_by_key(ENV['DASHING_TARGET_SPREAD_SHEET_ID']).worksheets[0]
+
+  a = ws[2,2]
+  send_event('aiben', { value: a  })
+end
